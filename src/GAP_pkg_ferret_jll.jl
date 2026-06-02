ad6ef33ed04ea3594286b8cffc502be51e2d22e0
jlbuild <juliabuildbot@gmail.com>
2026-06-02 18:19:40 +0000

    GAP_pkg_ferret_jll build 101.1600.0+0

--- src/GAP_pkg_ferret_jll.jl
+++ src/GAP_pkg_ferret_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_ferret")
-JLLWrappers.@generate_main_file("GAP_pkg_ferret", UUID("d6652ff9-bdf9-5f19-9e6f-c10727807222"))
+JLLWrappers.@generate_main_file("GAP_pkg_ferret", Base.UUID("d6652ff9-bdf9-5f19-9e6f-c10727807222"))
 end  # module GAP_pkg_ferret_jll
